f50d399ce8949e584b9478e8128284385e360744
darri <iaroslag@metropolia.fi>
2022-11-10 18:02:34 +0200

    1.4

--- 1.4.js
+++ 1.4.js
@@ -1,1 +1,18 @@
-let kohde = document.querySelector('#kohde')
+let kohde = document.querySelector('#kohde');
+let name = prompt('Give your name: ');
+let result = Math.floor(Math.random()*4+1);
+kohde.innerHTML = name + ', you are ' + result
+if (result == 1) {
+    result = 'Gryffindor';
+}
+else if (result == 2) {
+    result = 'Slytherin';
+}
+else if (result == 3) {
+    result = 'Hufflepuff';
+}
+else {
+    result = 'Ravenclaw';
+}
+kohde.innerHTML = name + ', you are ' + result
+
